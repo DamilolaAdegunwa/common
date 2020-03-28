e4d72fe1badad2affd7756c6db83af69215774c6
Damilola_Libmot <damilola.adegunwa@libmot.com>
2020-03-29 00:41:20 +0100

    commit

--- Authentication/Auth.API/Controllers/BasicController.cs
+++ Authentication/Auth.API/Controllers/BasicController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,7 @@ namespace Auth.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BasicController : ControllerBase
     {
         //I'd be writting some cool stuff here!!
