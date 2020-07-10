bad373f796eb0aae7fc170754f09347db8855637
Damilola Adegunwa <dammy.adegunwa@gmail.com>
2020-07-10 21:31:19 +0100

    commit

--- Miscellaneous/CodeSnippet.ConsoleApp/Program.cs
+++ Miscellaneous/CodeSnippet.ConsoleApp/Program.cs
@@ -1,208 +1,72 @@
 ﻿using CodeSnippet.ConsoleApp.Services;
+using Nito.AsyncEx;
 using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Numerics;
-using System.Runtime.InteropServices;
-using Newtonsoft.Json;
-using System.Linq.Expressions;
-using System.IO;
-using System.Web;
-using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
-using System.Runtime.Loader;
-using System.Reflection;
-using Newtonsoft.Json.Linq;
-using System.Timers;
-using Microsoft.Extensions.Primitives;
-using Microsoft.Extensions.FileProviders;
-using Polly;
-using Polly.Extensions.Http;
-using Microsoft.Extensions.Http;
-using System.Collections.Specialized;
-using static System.Text.StringBuilder;
-using System.Threading.Channels;
-using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
-using System.Collections.Concurrent;
-using System.Threading;
-using System.ComponentModel;
-using Nito.AsyncEx;
-//using Microsoft.Tpl.Dataflow;
-using System.Reactive;
-//using System.Reactive.Core;
-//using System.Reactive.Interfaces;
-using System.Reactive.Linq;
-using System.Reactive.PlatformServices;
-using Microsoft.Reactive.Testing;
-using EO.Base.UI;
-using System.Fabric.Description;
 
 namespace CodeSnippet.ConsoleApp
 {
-    public sealed class Program
+    public class Program
     {
-        #region parallel programming (to be cont'd)
-        //public static async Task Main()
-        //{
-        //    Action action = () => { Console.WriteLine("Done!!"); };
-        //    IObservable<string> observable = Observable.Return<string>("Damilola");
-        //    //Observable.Return("stub").Delay(Delay, Scheduler);
-        //    var t = new TaskCompletionSource<string>();
-        //    t.TrySetResult("doom!");
-        //    var tt = await t.Task;
-        //    StatefulServiceDescription d = Activator.CreateInstance<StatefulServiceDescription>();
-        //    var multiplyBlock = new TransformBlock<int, int>(item => item * 2);
-        //    var subtractBlock = new TransformBlock<int, int>(item => item - 2);
-        //    // After linking, values that exit multiplyBlock will enter subtractBlock.
-        //    multiplyBlock.LinkTo(subtractBlock);
-        //}
-        //IEnumerable<bool> PrimalityTest(IEnumerable<int> values)
-        //{
-        //    return values.AsParallel().Select(val => {
-        //        var x = 1;
-        //        var y = 2;
-        //        var z = 3;
+        public readonly int x = 4;
+        public static void Main()
+        {
+            #region ---
+            //ImmutableStack<T> ist = default;
+            ////System.Collections.Immutable includes many others!!
 
-        //        var a = 4;
-        //        var b = 5;
-        //        var c = 6;
-        //        return a == b;
-        //    }/*IsPrime(val)*/);
-        //}
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Observable.FromEventPattern<MouseEventHandler, MouseEventArgs>(
-        //    handler => (s, a) => handler(s, a),
-        //    handler => MouseMove += handler,
-        //    handler => MouseMove -= handler)
-        //    .Select(x => x.EventArgs.GetPosition(this))
-        //    .Sample(TimeSpan.FromSeconds(1))
-        //    .Subscribe(x => Trace.WriteLine(
-        //    DateTime.Now.Second + ": Saw " + (x.X + x.Y)));
-        //}
-        #endregion
+            //ConcurrentDictionary<string, int> cdtt = default;
+            //BlockingCollection<T> ts = default;
+            ////System.Collections.Concurrent ...
 
-        #region simple parallel programming 2
-        //static List<int> vsint = new int[] { 1, 2, 3, 4, 5, 6 }.ToList();//new List<int>();
-        //public static async Task Main()
-        //{
-        //    List<int> vsint = new List<int>();
-        //    Task.Run(() => { vsint = new int[] { 1, 2, 3, 4, 5, 6 }.ToList(); return "love!"; }).ContinueWith( tk => {
-        //        foreach (var j in vsint)
-        //        {
-        //            Console.WriteLine(j);
-        //        }
-        //    });
-        //    Console.WriteLine("done!");
-        //}
-        #endregion
+            //AsyncProducerConsumerQueue<T> asyncpc = default;
+            //AsyncCollection<T> asyncC = default;
+            ////Nito.AsyncEx ...
 
-        #region simple parallel programming 1
-        //public static async Task Main()
-        //{
-        //    var e = Enumerable.Range(1, 100000);
-        //    //var d = Channel.CreateUnbounded<int>();
-        //    List<int> vs = new List<int>();
-        //    Task.Run(async () =>
-        //    {
-        //        Parallel.ForEach(e, i =>
-        //        {
-        //            Task.Run(async () =>
-        //            {
-        //                //d.Writer.TryWrite(i);
-        //                vs.Add(i);
-        //            });
+            //BufferBlock<T> bb = default;
+            ////System.Threading.Tasks.Dataflow
 
-        //        });
-        //    });
-        //    while (true)
-        //    {
+            someService some = Activator.CreateInstance<someService>();
+            some.MyProperty = 125;
+            Console.WriteLine(some.MyProperty);
+            Console.ReadLine();
+            #endregion
+        }
+        public AsyncLazy<int> Data
+        {
+            get { return _data; }
+        }
+        public void Fetch(in int x, int y, in string z)
+        {
+            y = 10 + x;
 
-        //        Task.Run(async () =>
-        //        {
-        //            //Console.WriteLine(_ = (await d.Reader.ReadAsync()));
-        //            Console.WriteLine(vs.FirstOrDefault());
-        //        });
-        //        //break;
-        //    }
-        //}
-        #endregion
+            //x = 40; //compile time error!
+        }
 
-        #region Channel
-        //public static async Task Main()
-        //{
-        //    //BackgroundWorker backgroundWorker = new BackgroundWorker();
+        private readonly AsyncLazy<int> _data =
+        new AsyncLazy<int>(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return 13;
+        });
+        public static class AsyncInitialization
+        {
+            static Task WhenAllInitializedAsync(params object[] instances)
+            {
+                 return Task.WhenAll(Task.FromResult(instances.OfType<string>().Select(x => x)));
+            }
+        }
+    }
 
-        //    //var c = new MyQueue<int>();
-        //    var d = Channel.CreateUnbounded<int>();
-        //    _ = Task.Run(async delegate
-        //    {
-        //        for (var i = 0; true; i++)
-        //        {
-        //            await Task.Delay(100);
-        //            //c.Write(i);
-        //            d.Writer.TryWrite(i);
-        //        }
-        //    });
-        //    //var z = await d.Reader.ReadAllAsync().ToListAsync();// if your your aim is to get it all as a list
-        //    while (true)
-        //    {
-        //        //Console.WriteLine((_ = await c.ReadAsync()));
-        //        Console.WriteLine(_ = await d.Reader.ReadAsync());
-        //    }
-        //}
-
-        #endregion
-
-        #region Unsafe and Trials!
-        //public static void Main()
-        //{
-        //    
-        //    //public enum AccountType
-        //    //{
-        //    //    Android = 0, IOS = 1, Web = 2, Mobile = Android | IOS
-        //    //}
-        //    //string x = Activator.CreateInstance<string>();  
-        //    ////void x = ;
-        //    //AccountType accountType1 = AccountType.Android;
-        //    //AccountType accountType2 = AccountType.IOS;
-
-        //    //if(accountType1 == AccountType.Mobile && accountType2 == AccountType.Mobile)
-        //    //{
-        //    //    Console.WriteLine("both accounts are mobile!!");
-        //    //}else
-        //    //{
-        //    //    Console.WriteLine("they are not!!");
-        //    //}
-        //    //unsafe
-        //    //{
-        //    //    int a = 10;
-        //    //    int b = 20;
-        //    //    int c = 30;
-
-        //    //    int* ptra = &a;
-        //    //    int* ptrb = &b;
-        //    //    int* ptrc = &c;
-
-
-        //    //    Console.WriteLine((int)ptra);
-        //    //    Console.WriteLine(*ptra);
-
-
-        //    //}
-
-        //    ////string ptr = Convert.ToInt32(&x);
-        //    //byte[] byt = new byte[] { };
-        //    //BitConverter.ToInt32(byt);
-        //    //BinaryReader binaryReader = default;
-        //    
-
-        //    Console.WriteLine("done!");
-        //    Console.ReadLine();
-        //}
-        #endregion
+    public class someService
+    {
+        public someService()
+        {
+            Console.WriteLine("the constructor was called!");
+        }
+        public int MyProperty { get; set; }
     }
 }
--- Miscellaneous/CodeSnippet.ConsoleApp/Services/Snippets.cs
+++ Miscellaneous/CodeSnippet.ConsoleApp/Services/Snippets.cs
@@ -2,12 +2,90 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Description;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-
+using System.Collections.Generic;
+using System.Collections;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Linq.Expressions;
+using System.IO;
+using System.Web;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using System.Timers;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.FileProviders;
+using Polly;
+using Polly.Extensions.Http;
+using Microsoft.Extensions.Http;
+using System.Collections.Specialized;
+using static System.Text.StringBuilder;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.ComponentModel;
+using Nito.AsyncEx;
+//using Microsoft.Tpl.Dataflow;
+using System.Reactive;
+//using System.Reactive.Core;
+//using System.Reactive.Interfaces;
+using System.Reactive.Linq;
+using System.Reactive.PlatformServices;
+using Microsoft.Reactive.Testing;
+using EO.Base.UI;
+using System.Fabric.Description;
+using System.Fabric;
+//using Microsoft.Tpl.Dataflow;
+//using Microsoft.Bcl.Immutable;
+using System.Collections.Immutable;
 namespace CodeSnippet.ConsoleApp.Services
 {
+    public class LifetimeManager
+    {
+        FabricClient _client;
+        IReliableStateMangerReplica _stateMgr;
+        public LifetimeManager(IReliableStateMangerReplica stateMgr)
+        {
+            _stateMgr = stateMgr;
+            _client = new FabricClient();
+        }
+        public async Task<GameSession> CreateAsync(string serviceType, string pKey)
+        {
+            //var service = ServiceDescriptorFactory.Get(serviceType);
+            //if (service == null) throw new ArgumentException("cnt be found");
+            StatefulServiceDescription sd = new StatefulServiceDescription();
+            sd.ApplicationName = new Uri("www.DamilolaAdegunwa.com");
+            sd.HasPersistedState = true;
+            sd.MinReplicaSetSize = default;
+            sd.PartitionSchemeDescription = new SingletonPartitionSchemeDescription();
+            new UniformInt64RangePartitionSchemeDescription(1, 0, 0);
+
+            await _client.ServiceManager.CreateServiceAsync(sd);
+
+
+
+            return default;
+        }
+    }
+    public class GameSession
+    {
+    }
+
+    public interface IReliableStateMangerReplica
+    {
+    }
     public class Class1
     {
         public static void/*async Task*/ StringInterploationSample1()
@@ -44,6 +122,166 @@ namespace CodeSnippet.ConsoleApp.Services
         //this code deos not compile
         //void method2(int d, T t, string s);
     }
+    #region parallel programming (to be cont'd)
+    //public static async Task Main()
+    //{
+    //    Action action = () => { Console.WriteLine("Done!!"); };
+    //    IObservable<string> observable = Observable.Return<string>("Damilola");
+    //    //Observable.Return("stub").Delay(Delay, Scheduler);
+    //    var t = new TaskCompletionSource<string>();
+    //    t.TrySetResult("doom!");
+    //    var tt = await t.Task;
+    //    StatefulServiceDescription d = Activator.CreateInstance<StatefulServiceDescription>();
+    //    var multiplyBlock = new TransformBlock<int, int>(item => item * 2);
+    //    var subtractBlock = new TransformBlock<int, int>(item => item - 2);
+    //    // After linking, values that exit multiplyBlock will enter subtractBlock.
+    //    multiplyBlock.LinkTo(subtractBlock);
+    //}
+    //IEnumerable<bool> PrimalityTest(IEnumerable<int> values)
+    //{
+    //    return values.AsParallel().Select(val => {
+    //        var x = 1;
+    //        var y = 2;
+    //        var z = 3;
+
+    //        var a = 4;
+    //        var b = 5;
+    //        var c = 6;
+    //        return a == b;
+    //    }/*IsPrime(val)*/);
+    //}
+    //private void Button_Click(object sender, RoutedEventArgs e)
+    //{
+    //    Observable.FromEventPattern<MouseEventHandler, MouseEventArgs>(
+    //    handler => (s, a) => handler(s, a),
+    //    handler => MouseMove += handler,
+    //    handler => MouseMove -= handler)
+    //    .Select(x => x.EventArgs.GetPosition(this))
+    //    .Sample(TimeSpan.FromSeconds(1))
+    //    .Subscribe(x => Trace.WriteLine(
+    //    DateTime.Now.Second + ": Saw " + (x.X + x.Y)));
+    //}
+    #endregion
+
+    #region simple parallel programming 2
+    //static List<int> vsint = new int[] { 1, 2, 3, 4, 5, 6 }.ToList();//new List<int>();
+    //public static async Task Main()
+    //{
+    //    List<int> vsint = new List<int>();
+    //    Task.Run(() => { vsint = new int[] { 1, 2, 3, 4, 5, 6 }.ToList(); return "love!"; }).ContinueWith( tk => {
+    //        foreach (var j in vsint)
+    //        {
+    //            Console.WriteLine(j);
+    //        }
+    //    });
+    //    Console.WriteLine("done!");
+    //}
+    #endregion
+
+    #region simple parallel programming 1
+    //public static async Task Main()
+    //{
+    //    var e = Enumerable.Range(1, 100000);
+    //    //var d = Channel.CreateUnbounded<int>();
+    //    List<int> vs = new List<int>();
+    //    Task.Run(async () =>
+    //    {
+    //        Parallel.ForEach(e, i =>
+    //        {
+    //            Task.Run(async () =>
+    //            {
+    //                //d.Writer.TryWrite(i);
+    //                vs.Add(i);
+    //            });
+
+    //        });
+    //    });
+    //    while (true)
+    //    {
+
+    //        Task.Run(async () =>
+    //        {
+    //            //Console.WriteLine(_ = (await d.Reader.ReadAsync()));
+    //            Console.WriteLine(vs.FirstOrDefault());
+    //        });
+    //        //break;
+    //    }
+    //}
+    #endregion
+
+    #region Channel
+    //public static async Task Main()
+    //{
+    //    //BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+    //    //var c = new MyQueue<int>();
+    //    var d = Channel.CreateUnbounded<int>();
+    //    _ = Task.Run(async delegate
+    //    {
+    //        for (var i = 0; true; i++)
+    //        {
+    //            await Task.Delay(100);
+    //            //c.Write(i);
+    //            d.Writer.TryWrite(i);
+    //        }
+    //    });
+    //    //var z = await d.Reader.ReadAllAsync().ToListAsync();// if your your aim is to get it all as a list
+    //    while (true)
+    //    {
+    //        //Console.WriteLine((_ = await c.ReadAsync()));
+    //        Console.WriteLine(_ = await d.Reader.ReadAsync());
+    //    }
+    //}
+
+    #endregion
+
+    #region Unsafe and Trials!
+    //public static void Main()
+    //{
+    //    
+    //    //public enum AccountType
+    //    //{
+    //    //    Android = 0, IOS = 1, Web = 2, Mobile = Android | IOS
+    //    //}
+    //    //string x = Activator.CreateInstance<string>();  
+    //    ////void x = ;
+    //    //AccountType accountType1 = AccountType.Android;
+    //    //AccountType accountType2 = AccountType.IOS;
+
+    //    //if(accountType1 == AccountType.Mobile && accountType2 == AccountType.Mobile)
+    //    //{
+    //    //    Console.WriteLine("both accounts are mobile!!");
+    //    //}else
+    //    //{
+    //    //    Console.WriteLine("they are not!!");
+    //    //}
+    //    //unsafe
+    //    //{
+    //    //    int a = 10;
+    //    //    int b = 20;
+    //    //    int c = 30;
+
+    //    //    int* ptra = &a;
+    //    //    int* ptrb = &b;
+    //    //    int* ptrc = &c;
+
+
+    //    //    Console.WriteLine((int)ptra);
+    //    //    Console.WriteLine(*ptra);
+
+
+    //    //}
+
+    //    ////string ptr = Convert.ToInt32(&x);
+    //    //byte[] byt = new byte[] { };
+    //    //BitConverter.ToInt32(byt);
+    //    //BinaryReader binaryReader = default;
+    //    
+
+    //    Console.WriteLine("done!");
+    //    Console.ReadLine();
+    //}
+    #endregion
     #region orchard
     //[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     //public class BackgroundTaskAttribute : Attribute
