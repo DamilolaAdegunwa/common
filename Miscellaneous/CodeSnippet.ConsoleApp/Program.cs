b3655687ed6164d4b37c6d660aa2efb78e51f73f
Damilola Adegunwa <dammy.adegunwa@gmail.com>
2020-07-20 09:47:25 +0100

    commit

--- Miscellaneous/CodeSnippet.ConsoleApp/Program.cs
+++ Miscellaneous/CodeSnippet.ConsoleApp/Program.cs
@@ -6,7 +6,16 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
-
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+//using System.Drawing.Common;
+using System.Runtime.Remoting;
+using System.Runtime;
+using System.Diagnostics;
+//using Microsoft.;
+using FireBase.Notification;
+using FireBase;
 namespace CodeSnippet.ConsoleApp
 {
     public class Program
@@ -14,6 +23,20 @@ namespace CodeSnippet.ConsoleApp
         public readonly int x = 4;
         public static void Main()
         {
+            
+            using (var firebase = new FireBase.Notification.Firebase())
+            {
+                firebase.ServerKey = "{Your Server Key}";
+                var id = "{Your Device Id}";
+                firebase.PushNotifyAsync(id, "Hello", "World").Wait();
+                Console.ReadLine();
+            }
+            //autolayout
+
+            //    class
+            //sealed
+            //AnsiClass
+            //BeforeFieldInit
             #region ---
             //ImmutableStack<T> ist = default;
             ////System.Collections.Immutable includes many others!!
@@ -34,6 +57,33 @@ namespace CodeSnippet.ConsoleApp
             Console.WriteLine(some.MyProperty);
             Console.ReadLine();
             #endregion
+        }
+        void DoLongOperation()
+        {
+            var operationId = Guid.NewGuid();
+            //CallContext.LogicalSetData("OperationId", operationId);
+            DoSomeStepOfOperation();
+            //CallContext.FreeNamedDataSlot("OperationId");
+        }
+        void DoSomeStepOfOperation()
+        {
+            // Do some logging here.
+            //Trace.WriteLine("In operation: " + CallContext.LogicalGetData("OperationId"));
+        }
+        IEnumerable<int> ParallelMultiplyBy2(IEnumerable<int> values)
+        {
+            return values.AsParallel()
+            .WithDegreeOfParallelism(10)
+            .Select(item => item * 2);
+        }
+        // Using the Parallel class
+        void ParallelRotateMatrices(IEnumerable<Matrix> matrices, float degrees)
+        {
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 10
+            };
+            Parallel.ForEach(matrices, options, matrix => matrix.Rotate(degrees));
         }
         public AsyncLazy<int> Data
         {
