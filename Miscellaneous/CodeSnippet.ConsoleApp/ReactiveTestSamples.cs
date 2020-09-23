c8faec913e880c5067192ba28299e21863436ac3
Damilola Adegunwa <dammy.adegunwa@gmail.com>
2020-09-23 21:48:09 +0100

    commit

---
+++ Miscellaneous/CodeSnippet.ConsoleApp/ReactiveTestSamples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive;
+using System.Linq;
+using System.Threading;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+namespace CodeSnippet.ConsoleApp
+{
+    public class ReactiveTestSamples
+    {
+        public static void Sample1()
+        {
+            var query = from number in Enumerable.Range(1, 5) select number;
+            foreach(var number in query)
+            {
+                Console.WriteLine(number);
+            }
+            ImDone();
+            var observableQuery = query.ToObservable();
+        }
+        public static void ImDone()
+        {
+            Console.WriteLine("I'm done!");
+        }
+    }
+}
