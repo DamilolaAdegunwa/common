52ee137db4f11e9408fc69c5e9223a1a5920518b
Damilola Adegunwa <adegunwad@accessbankplc.com>
2021-03-30 09:29:44 +0100

    commit

---
+++ Engine/CodeSnippet.ConsoleApp/3geo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+namespace CodeSnippet.ConsoleApp
+{
+    public class test
+    {
+        public static void Main()
+        {
+            //var x = countTriplets(new List<long> { 1, 5, 5, 25, 125 }, 5);
+            //Console.WriteLine(x);
+            dammy: if (true) { }
+        }
+        static long countTriplets(List<long> arr, long r)
+        {
+            //List<string> sarr = arr.ToList();
+            long result = 0;
+            long s2 = 0;
+            long s3 = 0;
+            List<string> vs = new List<string>();
+            string v = "";
+            for (var i = 0; i < arr.Count; i++)
+            {
+                s2 = arr.Where(a => a == arr[i] * r).Count();
+                for (var j = 0; j < s2; j++)
+                {
+                    s3 = arr.Where(a => a == arr[i] * r * r).Count();
+                    for (var k = 0; k < s3; k++)
+                    {
+                        var a3 = new int[3] { i, j, k }.ToList();
+                        a3.Sort();
+                        v = $"{a3[0]},{a3[1]},{a3[2]}";
+                        if (!vs.Contains(v))
+                        {
+                            vs.Add(v);
+                            result++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- Engine/CodeSnippet.ConsoleApp/checkMagazine.cs
+++ Engine/CodeSnippet.ConsoleApp/checkMagazine.cs
@@ -1,14 +1,78 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
+using System;
 
 namespace CodeSnippet.ConsoleApp
 {
     public class checkMagazine
     {
-        public static void Main()
+        public static void MaincheckMagazine()
         {
+            //case 1
+            string[] m1 = "give me one grand today night".Split(' ');
+            string[] n1 = "give one grand today".Split(' ');
+            //answer : Yes
+            CheckMagazine(m1, n1);
 
+            //case 2
+            string[] m2 = "two times three is not four".Split(' ');
+            string[] n2 = "two times two is four".Split(' ');
+            //answer : No
+            CheckMagazine(m2, n2);
+
+            //case 3
+            string[] m3 = "ive got a lovely bunch of coconuts".Split(' ');
+            string[] n3 = "ive got some coconuts".Split(' ');
+            //answer : No
+            CheckMagazine(m3, n3);
+        }
+        static void CheckMagazine(string[] magazine, string[] note)
+        {
+            int index = -1;
+            List<string> mlist = magazine.ToList();
+            foreach (var w in note)
+            {
+                if (mlist.Contains(w))
+                {
+                    index = mlist.IndexOf(w);
+                    mlist.RemoveAt(index);
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+        static string isValid(string s)
+        {
+            var sarr = s.ToList();
+            var ds = s.Distinct().ToList();
+            int i = 0;
+            while (sarr.Count != 0 && sarr.Count != 1)
+            {
+                foreach (var x in ds)
+                {
+                    i = sarr.IndexOf(x);
+                    sarr.RemoveAt(i);
+                }
+                if (sarr.Count > 1 && sarr.Count < ds.Count())
+                {
+                    return "NO";
+                }
+            }
+            return "YES";
         }
     }
 }
