20556b107529986423d093b445e2128f9ce52767
Damilola Adegunwa <adegunwad@accessbankplc.com>
2021-07-30 15:02:08 +0100

    commit

---
+++ Engine/SignalrClient.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalrClient.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
