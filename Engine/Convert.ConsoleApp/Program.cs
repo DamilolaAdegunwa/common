bbc7ed1d463ddf6be8ee940a9dc07c6a0a81831d
Damilola Adegunwa <dammy.adegunwa@gmail.com>
2023-04-25 14:15:21 +0100

    save

--- Engine/Convert.ConsoleApp/Program.cs
+++ Engine/Convert.ConsoleApp/Program.cs
@@ -12,8 +12,10 @@ namespace ConvertApp.ConsoleApp
 	{
 		public static void Main() 
 		{
-			BenchmarkRunner.Run<SimpleSpanTest>();
-			Console.WriteLine("program file up and running!");
+			ReadOnlySpanTest<int> i = new ReadOnlySpanTest<int>();// default(ReadOnlySpanTest<int>);
+			Console.WriteLine($"{i.Age}");
+			Console.WriteLine($"{i.YearsOfExperience}");
+			Console.WriteLine($"{i.Strings.ToArray()}");
 		}
 	}
 }
--- Engine/Convert.ConsoleApp/Services/SystemSpan/SimpleSpanTest.cs
+++ Engine/Convert.ConsoleApp/Services/SystemSpan/SimpleSpanTest.cs
@@ -1,17 +1,22 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConvertApp.ConsoleApp.Services.SystemSpan
 {
+	#region run in main program 1
+
+	#endregion
 	[MemoryDiagnoser]
 	public class SimpleSpanTest
 	{
+		//public List<ReadOnlySpanTest> readOnlySp { get; set; }
 		private static readonly string _dateAsText = "08 07 2023";
-		private static readonly ReadOnlySpan<string> strings;
+		//private readonly ReadOnlySpan<string> strings;
 		public SimpleSpanTest() { }
 
 		[Benchmark]
@@ -49,8 +54,39 @@ namespace ConvertApp.ConsoleApp.Services.SystemSpan
 		}
 	}
 
-	public readonly ref struct ReadOnlySpanTest<T>
+	public interface INothing
 	{
 
+	}
+	public readonly ref partial struct ReadOnlySpanTest<T> //: INothing //cannot impl interface
+	{
+		public readonly int Age { get; }
+		public readonly int YearsOfExperience { get; }
+
+		public readonly ReadOnlySpan<string> Strings = new string[] { };
+		//public ReadOnlySpanTest(int age, int yoe, string[] s) => (Age, YearsOfExperience, Strings) = (age, yoe, s);
+		public ReadOnlySpanTest(int age, int yoe, ReadOnlySpan<string> s)
+		{
+			Age = age;
+			YearsOfExperience = yoe;
+			Strings = s;
+		}
+
+		public ReadOnlySpanTest<T> Test()
+		{
+			return default(ReadOnlySpanTest<T>);
+		}
+
+		public static ReadOnlySpanTest<T> Test2()
+		{
+			return default(ReadOnlySpanTest<T>);
+		}
+
+		public ReadOnlySpanTest()
+		{
+			Age = 35;
+			YearsOfExperience = 10;
+			Strings = new string[] { "One", "Two", "Three", "Four", "Five", "Six" };
+		}
 	}
 }
