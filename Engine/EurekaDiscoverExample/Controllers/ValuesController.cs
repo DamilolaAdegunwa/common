eb6d9d37b0e200754f0740bf6e7059209e47d298
Oluwadamilola Adegunwa <Oluwadamilola.adegunwa@outlook.com>
2023-07-13 17:12:50 +0100

    c

---
+++ Engine/EurekaDiscoverExample/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+using System.Net.Http;
+using Steeltoe.Discovery;
+
+namespace EurekaDiscoverExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        DiscoveryHttpClientHandler _handler;
+        public ValuesController(ILogger<ValuesController> logger, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient(_handler, false);
+            return await client.GetStringAsync("http://EurekaRegisterExample/api/values");
+        }
+    }
+}
